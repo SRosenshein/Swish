52f5586042f26d0f5aa58bf6603beb75da0e44fd
Sam Rosenshein <srosenshein1@gmail.com>
2016-12-27 18:56:12 -0500

    adds react router setup

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Home = React.createClass ({
+	render() {
+		return (
+			<div>
+				<h1>Home Page</h1>
+			</div>
+		)
+	}
+});
+
+export default Home;
--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const HelloWorld = React.createClass({
-	render() {
-		return (
-			<div> Hello! Dwellings </div>
-		)
-	}
-});
+//Import components
+import Main from './components/Main';
+import Home from './components/Home';
 
-render(
-	<HelloWorld />, document.getElementById('app')
-);
+//Import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={Home}></IndexRoute>
+		</Route>
+	</Router>
+)
+
+render(router, document.getElementById('app'));
