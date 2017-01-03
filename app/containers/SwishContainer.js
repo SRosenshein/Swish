37ac19b2afda2ba810936b7f152d976ee3d64302
Sam Rosenshein <srosenshein1@gmail.com>
2017-01-03 12:37:23 -0500

    adds main app container

---
+++ app/containers/SwishContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+// Import components
+
+const SwishContainer = React.createClass({
+	render() {
+		return (
+			//<Component 1 />
+			//<Component 2 />
+			//<Component 3 />
+			//<Component 4 />
+			<div>Main App Page</div>
+		)
+	}
+});
+
+export default SwishContainer;
