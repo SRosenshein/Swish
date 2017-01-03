5f75a8547045c58354c55c4110f1895cb000bcf4
Sam Rosenshein <srosenshein1@gmail.com>
2017-01-03 10:46:18 -0500

    adds landing page

--- app/components/Main.js
+++ app/components/Main.js
@@ -11,9 +11,15 @@ const Main = React.createClass ({
 						<div className="navbar-header">
 							<Link to="/"><span className="navbar-brand" style={styles.brand}><em>Swish</em></span></Link>
 						</div>
-						<div className="collapse navbar-collapse navbar-right">
+						<div className="collapse navbar-collapse">
 							<ul className="nav navbar-nav">
 								<Link to="/about"><li style={styles.about}>About</li></Link>
+							</ul>
+						</div>
+						<div className="collapse navbar-collapse navbar-right">
+							<ul className="nav navbar-nav">
+								<li>Sign Up</li>
+								<li>Log In</li>
 							</ul>
 						</div>
 					</div>
